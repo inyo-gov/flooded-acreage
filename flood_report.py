b7334b18876ab33d82d2190e7e9660411feee9ec
Zach Nelson <znelson@inyocounty.us>
2025-03-05 07:04:13 -0800

    fix .csv download link

--- flood_report.py
+++ flood_report.py
@@ -379,7 +379,7 @@ def main(start_date, threshold):
             <p>Flooded acres were calculated from Sentinel-2 Surface Reflectance imagery using the Earth Engine Python API.</p>
             <p>NIR band was used to identify flooded areas by applying a threshold to isolate water.</p>
             <p>Vectorized flooded extent boundaries - GeoJSON <a href="clipped_flooded_areas_{image_date_str}_{threshold}.geojson" download>here</a>.</p>
-            <p>Flooded extent CSV <a href="csv_output/flood_report_data_{image_date_str}_{threshold}.geojson" download>here</a>.</p>
+            <p>Flooded extent CSV <a href="csv_output/flood_report_data_{image_date_str}_{threshold}.csv" download>here</a>.</p>
             <p>Sentinel 2 false color composite GeoTIFF <a href="false_color_composite_{image_date_str}_{threshold}.tif" download>here</a>.</p>
         </div>
         </div>
